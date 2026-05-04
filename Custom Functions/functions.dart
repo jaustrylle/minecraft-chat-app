afa98849203f1abca566182eef5925675b3e6753
jaustrylle <sanren.reese@gmail.com>
2026-05-03 20:43:50 -0500

    Update functions.dart

--- Custom Functions/functions.dart
+++ Custom Functions/functions.dart
@@ -1,19 +1,21 @@
 import 'dart:convert';
 import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:timeago/timeago.dart' as timeago;
-import '/flutter_flow/custom_functions.dart';
-import '/flutter_flow/lat_lng.dart';
-import '/flutter_flow/place.dart';
-import '/flutter_flow/uploaded_file.dart';
-import '/backend/supabase/supabase.dart';
-import '/auth/supabase_auth/auth_util.dart';
+import 'lat_lng.dart';
+import 'place.dart';
+import 'uploaded_file.dart';
+import '/backend/backend.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '/auth/firebase_auth/auth_util.dart';
 
-int likes(dynamic post) {
-  /// MODIFY CODE ONLY BELOW THIS LINE
+int likes(UserPostsRecord? post) {
   return post!.likes!.length;
+}
 
-  /// MODIFY CODE ONLY ABOVE THIS LINE
+bool hasUploadedMedia(String? mediaPath) {
+  return mediaPath != null && mediaPath.isNotEmpty;
 }
