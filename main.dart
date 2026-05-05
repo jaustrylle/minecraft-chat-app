1070918c7ca69571a84f6342dc4a34b433465724
jaustrylle <sanren.reese@gmail.com>
2026-05-05 01:07:42 -0500

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'pages/login.dart'; // IMPORTANT (fixes your error)
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'App',
+      theme: ThemeData.light(),
+      home: const LoginPage(), // or MainFeedWidget after login
+    );
+  }
+}
